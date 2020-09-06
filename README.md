b1b4007d8e9fc0ff8006552dcecc0102233975d3
Lukman Hakim <tjg.lukman@gmail.com>
2020-09-06 15:11:26 +0700

    init

---
+++ README.md
@@ -0,0 +1,56 @@
+[build]: https://img.shields.io/badge/build-success-success
+# Spring Data Rest Example
+ 
+![Build][build]
+
+#### Technology
+
+- Lang : `Java`
+- Framework : `Spring Framework`
+- Database : `H2 (Embedded)`
+
+---
+
+#### Requirements
+
+- JDK 8
+- Maven 3
+
+---
+
+#### Endpoints
+
+| Swagger | http://localhost:8080/swagger-ui/ |
+| H2 Console | http://localhost:8080/db |
+
+---
+
+#### Run
+
+```
+$ mvn clean spring-boot:run
+```
+
+---
+
+#### Build
+
+* Jar
+    ```
+    $ mvn clean install
+    ```
+    Output Location : 
+    `target/demo-0.0.1-SNAPSHOT.jar` 
+
+* Docker Image
+    ```
+    $ mvn spring-boot:build-image
+    ```
+---
+
+#### Deploy
+
+```
+Lorem Ipsum
+```
+
---
+++ src/main/java/com/example/demo/App.java
@@ -0,0 +1,91 @@
+package com.example.demo;
+
+import com.github.javafaker.Faker;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+@EnableSwagger2
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    ApplicationRunner inject(EmployeeRepository repository) {
+        Faker faker = new Faker(Locale.US);
+        List<Employee> employees = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            String fullName = faker.name().fullName();
+            String email = fullName.replaceAll("[^a-zA-Z ]", "").replace(" ", ".");
+            employees.add(Employee.builder()
+                    .fullName(fullName)
+                    .email(email)
+                    .address(faker.address().fullAddress())
+                    .phone(faker.phoneNumber().cellPhone())
+                    .job(faker.job().position())
+                    .build());
+        }
+        return args -> repository.saveAll(employees);
+    }
+
+    @Bean
+    Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(new ApiInfoBuilder().title("Demo App").build())
+                .select()
+                .paths(PathSelectors.regex("/error").negate())
+                .paths(PathSelectors.regex("/profile").negate())
+                .paths(PathSelectors.ant("/actuator/**").negate())
+                .build();
+    }
+}
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+class Employee implements Serializable {
+
+    private static final long serialVersionUID = 5161632694371701599L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String address;
+    private String phone;
+    private String job;
+}
+
+@RepositoryRestResource
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    @RestResource(path = "name")
+    List<Employee> findByFullNameContainingIgnoreCase(String q);
+}
